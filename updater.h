128a3572c93d70ce437f0ab23593bfc047278d7e
pirosiki197 <lingmukangtai825@gmail.com>
2025-05-12 11:34:25 +0900

    Add initial definitions for Tetris game structures and operations

---
+++ updater.h
@@ -0,0 +1,36 @@
+typedef enum {
+    T,
+    S,
+    Z,
+    L,
+    J,
+    O,
+    I,
+} TetrisType;
+
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+typedef struct {
+    TetrisType type;
+    Position position;
+    bool block[4][4];
+} Mino;
+
+typedef enum {
+    Right,
+    Left,
+    RotateLeft,
+    RotateRight,
+    Drop,
+} Operation;
+
+Mino move_mino(Mino current, Operation op);
+
+bool is_valid_state(bool field[20][10], Mino mino);
+
+int get_complete_line(bool field[20][10]);
+
+void update_field(bool **field);
