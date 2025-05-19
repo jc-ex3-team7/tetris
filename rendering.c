bf8ac42dbcfff952791cf06539941e59b1e69844
tagucha <tagucha.dev@gmail.com>
2025-05-19 11:52:26 +0900

    refactor: enhance rendering logic with cursor movement and buffer management

--- rendering.c
+++ rendering.c
@@ -121,8 +121,44 @@ int color_to_code(char *out, Color color) {
 static char RENDER_BUFFER[BUFFER_SIZE];
 static int RENDER_BUFFER_INDEX = 0;
 static Color COLOR_FIELD[22][12];
+static Color PREV_COLOR_FIELD[22][12];
+static bool first_render = true;
 
-// 出力をバッファリングし、必要時にフラッシュ
+// 簡易的整数→文字列変換
+static int int_to_str(int num, char *buf) {
+    int len = 0;
+    int temp = num;
+    char rev[10];
+    if (num == 0) {
+        buf[len++] = '0';
+        buf[len] = '\0';
+        return 1;
+    }
+    while (temp > 0) {
+        rev[len++] = '0' + (temp % 10);
+        temp /= 10;
+    }
+    for (int i = 0; i < len; ++i) {
+        buf[i] = rev[len - 1 - i];
+    }
+    buf[len] = '\0';
+    return len;
+}
+
+// カーソル移動コード生成
+static int move_cursor_code(char *out, int row, int col) {
+    int idx = 0;
+    out[idx++] = '\x1b';
+    out[idx++] = '[';
+    idx += int_to_str(row, out + idx);
+    out[idx++] = ';';
+    idx += int_to_str(col, out + idx);
+    out[idx++] = 'H';
+    out[idx] = '\0';
+    return idx;
+}
+
+// 出力バッファリング
 void buffered_print(const char *arg, int len, bool flush) {
     if (RENDER_BUFFER_INDEX + len >= BUFFER_SIZE) {
         RENDER_BUFFER[RENDER_BUFFER_INDEX] = '\0';
@@ -140,16 +176,9 @@ void buffered_print(const char *arg, int len, bool flush) {
 
 // 描画関数
 void render(State state) {
-    // バッファ初期化
-    memset(RENDER_BUFFER, 0, BUFFER_SIZE);
-    RENDER_BUFFER_INDEX = 0;
-    // 画面クリア
-    buffered_print("\x1b[2J", 4, false);
-    buffered_print("\x1b[H", 3, false);
-
-    // フィールドの枠をセット
-    for (int y = 0; y < 22; y++) {
-        for (int x = 0; x < 12; x++) {
+    // フィールドの準備
+    for (int y = 0; y < 22; ++y) {
+        for (int x = 0; x < 12; ++x) {
             if (y == 0 || y == 21 || x == 0 || x == 11) {
                 COLOR_FIELD[y][x] = WHITE;
             } else {
@@ -157,70 +186,94 @@ void render(State state) {
             }
         }
     }
-
-    // テトリミノのマージ
-    for (int y = 0; y < 20; y++) {
-        for (int x = 0; x < 10; x++) {
+    for (int y = 0; y < 20; ++y) {
+        for (int x = 0; x < 10; ++x) {
             if (state.field[y][x]) {
                 COLOR_FIELD[y + 1][x + 1] = WHITE;
             }
         }
     }
-
-    // テトリミノの描画
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
             if (state.mino.block[i][j]) {
-                int y = state.mino.position.y + i;
-                int x = state.mino.position.x + j;
+                int yy = state.mino.position.y + i + 1;
+                int xx = state.mino.position.x + j + 1;
                 switch (state.mino.type) {
                     case T:
-                        COLOR_FIELD[y + 1][x + 1] = MAGENTA;
+                        COLOR_FIELD[yy][xx] = MAGENTA;
                         break;
                     case S:
-                        COLOR_FIELD[y + 1][x + 1] = GREEN;
+                        COLOR_FIELD[yy][xx] = GREEN;
                         break;
                     case Z:
-                        COLOR_FIELD[y + 1][x + 1] = RED;
+                        COLOR_FIELD[yy][xx] = RED;
                         break;
                     case L:
-                        COLOR_FIELD[y + 1][x + 1] = ORANGE;
+                        COLOR_FIELD[yy][xx] = ORANGE;
                         break;
                     case J:
-                        COLOR_FIELD[y + 1][x + 1] = BLUE;
+                        COLOR_FIELD[yy][xx] = BLUE;
                         break;
                     case O:
-                        COLOR_FIELD[y + 1][x + 1] = YELLOW;
+                        COLOR_FIELD[yy][xx] = YELLOW;
                         break;
                     case I:
-                        COLOR_FIELD[y + 1][x + 1] = CYAN;
+                        COLOR_FIELD[yy][xx] = CYAN;
                         break;
                 }
             }
         }
     }
 
-    // 描画ループ
-    char color_buffer[16];
-    int buffered_len = 0;
-    Color prev_color = WHITE;
-    // バッファに色をセット
-    for (int y = 0; y < 22; y++) {
-        prev_color = WHITE;
-        buffered_len = color_to_code(color_buffer, WHITE);
-        buffered_print(color_buffer, buffered_len, false);
-        for (int x = 0; x < 12; x++) {
-            if (COLOR_FIELD[y][x] != prev_color) {
-                buffered_len = color_to_code(color_buffer, COLOR_FIELD[y][x]);
-                buffered_print(color_buffer, buffered_len, false);
-                prev_color = COLOR_FIELD[y][x];
+    int y, x, len;
+    char color_buf[16], cs_buf[16];
+
+    if (first_render) {
+        // 全描画
+        memset(RENDER_BUFFER, 0, BUFFER_SIZE);
+        RENDER_BUFFER_INDEX = 0;
+        buffered_print("\x1b[2J", 4, false);
+        buffered_print("\x1b[H", 3, false);
+        for (y = 0; y < 22; ++y) {
+            Color prev = WHITE;
+            len = color_to_code(color_buf, WHITE);
+            buffered_print(color_buf, len, false);
+            for (x = 0; x < 12; ++x) {
+                if (COLOR_FIELD[y][x] != prev) {
+                    len = color_to_code(color_buf, COLOR_FIELD[y][x]);
+                    buffered_print(color_buf, len, false);
+                    prev = COLOR_FIELD[y][x];
+                }
+                buffered_print("  ", 2, false);
             }
-            buffered_print("  ", 2, false);
+            buffered_print("\x1b[0m\n", 5, false);
         }
-        // 行末にリセットと改行を挿入
-        buffered_print("\x1b[0m\n", 5, false);
+        buffered_print("", 0, true);
+    } else {
+        // 差分更新
+        for (y = 0; y < 22; ++y) {
+            for (x = 0; x < 12; ++x) {
+                if (COLOR_FIELD[y][x] != PREV_COLOR_FIELD[y][x]) {
+                    len = move_cursor_code(cs_buf, y + 1, x * 2 + 1);
+                    buffered_print(cs_buf, len, false);
+                    len = color_to_code(color_buf, COLOR_FIELD[y][x]);
+                    buffered_print(color_buf, len, false);
+                    buffered_print("  ", 2, false);
+                }
+            }
+        }
+        buffered_print("\x1b[0m", 4, false);
+        // カーソルを画面下に
+        len = move_cursor_code(cs_buf, 23, 1);
+        buffered_print(cs_buf, len, false);
+        buffered_print("", 0, true);
     }
 
-    // 最終フラッシュ
-    buffered_print("", 0, true);
+    // 前回バッファを更新
+    for (y = 0; y < 22; ++y) {
+        for (x = 0; x < 12; ++x) {
+            PREV_COLOR_FIELD[y][x] = COLOR_FIELD[y][x];
+        }
+    }
+    first_render = false;
 }
